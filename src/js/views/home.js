2c41cad8735e5a9a75b278a59b67fda4cb9bb62e
Anna Jackson <106778075+GorgGoddess@users.noreply.github.com>
2022-10-20 16:34:58 -0500

    first commit

---
+++ src/js/views/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+import Card from "../component/Card/card";
+
+export const Home = () => (
+	<div className="text-center mt-5">
+		<Card />
+	</div>
+);
